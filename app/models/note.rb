2a5ec66b96486679ba3f61e7c377b05c00e2f538
Matt Swezey <mattswezey@Matts-MacBook-Pro.local>
2010-11-03 17:47:22 -0500

    bug fixes and global chat

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,12 +4,14 @@ class Note < ActiveRecord::Base
   belongs_to :user
   belongs_to :shipment
   
-  validates_presence_of :user, :shipment, :content
+  validates_presence_of :user, :content
+  
+  default_scope :order => 'created_at DESC'
   
   after_create :email_users
   
   def email_users
-    Notifier.deliver_new_note(self) if shipment.users.size > 0
+    Notifier.deliver_new_note(self) if shipment && shipment.users.size > 0
   end
   
 end
